128893e339f59b67937e8edcca1910d902deee54
MD Rasel Backend Dev. <rasel.mamun314@gmail.com>
2026-03-31 02:25:59 +0600

    doment add

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: [
+      'plasticlettersandsigns.com',
+      'www.plasticlettersandsigns.com',
+    ],
     remotePatterns: [
       {
         protocol: 'http',
